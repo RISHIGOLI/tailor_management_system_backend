f18b8da51a94d6afcf49f906310d2b712bedab1e
RISHI GOLI <rg0001.ayconnects@gmail.com>
2024-07-25 15:38:23 +0530

    first commit

---
+++ src/main/java/com/tailor/admin/TailorManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.tailor.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TailorManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TailorManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tailor/admin/TailorManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tailor.admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TailorManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
